d4c4f245b40b773664a8f48e73a770250f35133e
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-03-01 09:59:06 +0530

    payment update

--- Back_End/src/main/java/lk/ijse/Spring/Controller/PurchaseOrderController.java
+++ Back_End/src/main/java/lk/ijse/Spring/Controller/PurchaseOrderController.java
@@ -1,5 +1,6 @@
 package lk.ijse.Spring.Controller;
 
+import lk.ijse.Spring.dto.CarDto;
 import lk.ijse.Spring.dto.OrdersDTO;
 import lk.ijse.Spring.entity.OrderDetails;
 import lk.ijse.Spring.service.PurchaseOrderService;
@@ -7,6 +8,8 @@ import lk.ijse.Spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/purchase")
@@ -28,10 +31,18 @@ public class PurchaseOrderController {
     }
 
 
-    @GetMapping(path = "/{oid}")
-    public ResponseUtil searchOrder(@PathVariable String oid) {
-        OrdersDTO ordersDTO = service.searchOrder(oid);
-        return new ResponseUtil("200", "Successfully Purchased.!", ordersDTO);
+//    @GetMapping(path = "/{oid}")
+//    public ResponseUtil searchOrder(@PathVariable String oid) {
+//        OrdersDTO ordersDTO = service.searchOrder(oid);
+//        return new ResponseUtil("200", "Successfully Purchased.!", ordersDTO);
+//    }
+
+    @GetMapping
+    public ResponseUtil getAllOrder(){
+        ArrayList<OrdersDTO> ordersDTOS = service.getAllOrders();
+        System.out.println(ordersDTOS);
+        return new ResponseUtil("200"," Success",ordersDTOS);
+
     }
 
 }
--- Back_End/src/main/java/lk/ijse/Spring/service/impl/PurchaseOrderServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/Spring/service/impl/PurchaseOrderServiceImpl.java
@@ -1,15 +1,19 @@
 package lk.ijse.Spring.service.impl;
 
+import lk.ijse.Spring.dto.CarDto;
+import lk.ijse.Spring.dto.OrderDetailsDTO;
 import lk.ijse.Spring.dto.OrdersDTO;
 import lk.ijse.Spring.entity.OrderDetails;
 import lk.ijse.Spring.entity.Orders;
 import lk.ijse.Spring.repo.*;
 import lk.ijse.Spring.service.PurchaseOrderService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @Service
 @Transactional
@@ -52,8 +56,17 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
 
     @Override
     public OrdersDTO searchOrder(String oid) {
-        return mapper.map(ordersRepo.findById(oid), OrdersDTO.class);
+        return mapper.map(ordersRepo.findById(oid),
+                OrdersDTO.class);
     }
-
-
+    @Override
+    public ArrayList<OrdersDTO> getAllOrders(){
+        return mapper.map(ordersRepo.findAll(), new TypeToken<ArrayList<OrdersDTO>>() {
+        }.getType());
+    }
+    @Override
+    public ArrayList<OrderDetailsDTO> getAllOrdersDetails(){
+        return mapper.map(orderDetailsRepo.findAll(), new TypeToken<ArrayList<OrderDetailsDTO>>() {
+        }.getType());
+    }
 }
