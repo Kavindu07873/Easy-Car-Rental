ae5611cccbcf93249fccdd0de3ec554c714280d0
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-03-02 16:33:53 +0530

    update value2

--- Back_End/src/main/java/lk/ijse/Spring/repo/CarRepo.java
+++ Back_End/src/main/java/lk/ijse/Spring/repo/CarRepo.java
@@ -4,4 +4,6 @@ import lk.ijse.Spring.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarRepo extends JpaRepository<Car,String> {
+
+    Car findCarByType(String type);
 }
--- Back_End/src/main/java/lk/ijse/Spring/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/Spring/service/impl/CarServiceImpl.java
@@ -45,4 +45,7 @@ public class CarServiceImpl implements CarService {
         }.getType());
     }
 
+
+
+
 }
