5f073171282b6587613e925cab88c1db1b7b0b12
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-03-08 16:53:15 +0530

    finalize

---
+++ Back_End/src/main/java/lk/ijse/Spring/Controller/FinalizeController.java
@@ -0,0 +1,32 @@
+package lk.ijse.Spring.Controller;
+
+import lk.ijse.Spring.dto.Finalizedto;
+import lk.ijse.Spring.dto.OrdersDTO;
+import lk.ijse.Spring.service.FinalizeService;
+import lk.ijse.Spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Finalize")
+@CrossOrigin
+public class FinalizeController {
+
+    @Autowired
+    private FinalizeService finalizeService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil FinalizeOrder(@RequestBody Finalizedto dto) {
+
+        System.out.println(dto.toString());
+
+
+        finalizeService.FinalizeOrder(dto);
+        return new ResponseUtil("200", "Successfully Purchased.!", null);
+
+    }
+
+
+
+}
---
+++ Back_End/src/main/java/lk/ijse/Spring/service/FinalizeService.java
@@ -0,0 +1,9 @@
+package lk.ijse.Spring.service;
+
+import lk.ijse.Spring.dto.Finalizedto;
+
+public interface FinalizeService {
+    public void FinalizeOrder(Finalizedto dto);
+
+
+}
